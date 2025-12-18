944232e951c248bec71c4c202bbad62c675a1772
anukuncham2004 <anukuncham2004@gmail.com>
2025-12-18 14:16:30 +0530

    Add files via upload

---
+++ OneDimensionalArray.java
@@ -0,0 +1,13 @@
+public class OneDimensionalArray{
+public static void main(String[]args){
+Scanner s=new Scanner(System.in);
+int age[]=new int[5];
+for(int i=0;i<age.length;i++){
+System.out.println("enter the of the student"+i);
+age[i]=s.nextInt();
+}
+System.out.println("----------------the ages aree-----------");
+for(int i=0;i<age.length;i++){
+System.out.println(
+}
+}
