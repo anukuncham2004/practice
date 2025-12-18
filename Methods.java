bba45329929365084ca9e4bef603557eeacb3588
anukuncham2004 <anukuncham2004@gmail.com>
2025-12-18 14:22:05 +0530

    Add files via upload

---
+++ Methods.java
@@ -0,0 +1,15 @@
+class Demo{
+int a,b,c;
+void add(){
+a=10;
+b=20;
+c=a+b;
+System.out.println(c);
+}
+}
+public class Methods{
+public static void main(String[]args){
+Demo d=new Demo();
+d.add();
+}
+}
