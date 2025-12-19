516b0cdac945664b0cde92a016d1f417df7f9ada
anukuncham2004 <anukuncham2004@gmail.com>
2025-12-19 10:59:09 +0530

    Add files via upload

---
+++ DisadvantageOfMultithreading1.java
@@ -0,0 +1,33 @@
+class Printer implements Runnable
+{
+synchronized public void run(){
+String name=Thread.currentThread().getName();
+System.out.println(name + "printing started.");
+for(int i=1;i<=5;i++){
+synchronized(this){
+System.out.println(name +"is printing.");
+}
+try{
+Thread.sleep(2000);
+}
+catch(Exception e){
+System.out.println("Some problem occurred");
+}
+}
+System.out.println(name + "Completed the printing");
+}
+}
+public class DisadvantageOfMultithreading1{
+public static void main(String[]args){
+Printer p=new Printer();
+Thread t1=new Thread(p);
+Thread t2=new Thread(p);
+Thread t3=new Thread(p);
+t1.setName("Anu");
+t2.setName("Minnu");
+t3.setName("Sanvy");
+t1.start();
+t2.start();
+t3.start();
+}
+}
