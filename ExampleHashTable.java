26deb72ffa1e231493ae20a4770a9ec20242cb72
anukuncham2004 <anukuncham2004@gmail.com>
2025-12-21 17:45:17 +0530

    Add files via upload

---
+++ ExampleHashTable.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class ExampleHasTable{
+public static void main(String[]args){
+HashTable ht=new HashTable();
+ht.put("Name","Anu");
+ht.put("Age",25);
+System.out.println(ht);
+}
+}
