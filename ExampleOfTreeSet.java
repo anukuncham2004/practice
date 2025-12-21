9de760e127243ed19bda3efb984cd81366f8b4ee
anukuncham2004 <anukuncham2004@gmail.com>
2025-12-21 17:47:50 +0530

    Add files via upload

---
+++ ExampleOfTreeSet.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ExampleOfTreeSet{
+public static void main(String[]args){
+TreeSet<Integer> ts=new TreeSet<Integer>();
+ts.add(10);
+ts.add(20);
+ts.add(30);
+ts.add(50);
+ts.add(40);
+System.out.println(ts);
+}
+}
